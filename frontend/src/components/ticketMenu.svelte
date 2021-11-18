50a8f0fb0e8b338a915ce928b995b9dc939034d1
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-11-18 13:33:00 +0300

    bilet satın alma ekranı için hazırlıklar yapıldı

---
+++ frontend/src/components/ticketMenu.svelte
@@ -0,0 +1,12 @@
+<script>
+    let busImage = 'assets/bus.png';
+    let planeImage = 'assets/plane.png';
+</script>
+
+<main>
+    
+</main>
+
+<style>
+
+</style>
--- frontend/src/views/homePage.svelte
+++ frontend/src/views/homePage.svelte
@@ -1,11 +1,15 @@
 <script>
     import Navbar from '../components/navbar.svelte'
+    import TicketSection from '../components/ticketMenu.svelte'
 </script>
 
 <main>
     <Navbar />
-
+    <TicketSection />
     <!-- BİLET SATIN ALMA BÖLÜMÜ-->
+
+
+
 </main>
 
 <style>
