80a8eb38b2f4f2e5c0bf1ae5b9a6c21da1e0b349
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-11-18 13:14:35 +0300

    navbar bitti

--- frontend/src/views/homePage.svelte
+++ frontend/src/views/homePage.svelte
@@ -1,43 +1,11 @@
 <script>
-
+    import Navbar from '../components/navbar.svelte'
 </script>
 
 <main>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark py-3 fixed-top">
-        <div class="container">
-            <a href="#" class="navbar-brand">Online Bilet</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu"><span
-                    class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navmenu">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Bilet İptal</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Rezervasyonlarım</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Üye ol</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Giriş</a>
-                    </li>
-                    <li class="nav-item px-3">
-                        <select class="form-select form-select-sm" aria-label=".form-select-sm example">
-                            <option selected>TR</option>
-                            <option value="1">ENG</option>
-                        </select>
-                    </li>
-                    <li class="nav-item">
-                        <select class="form-select form-select-sm" aria-label=".form-select-sm example">
-                            <option selected>TL</option>
-                            <option value="1">USD</option>
-                        </select>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <Navbar />
+
+    <!-- BİLET SATIN ALMA BÖLÜMÜ-->
 </main>
 
 <style>
