ba9b96f35d2984a1fb2d2d9a668e9d31ee5910eb
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-11-20 12:08:39 +0300

    footer eklendi

---
+++ frontend/src/components/footer.svelte
@@ -0,0 +1,48 @@
+<script>
+
+</script>
+
+<main>
+    <div class="mt-5 pt-5 pb-5 footer bg-dark text-white">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-xs-12 about-company">
+                    <h2>Heading</h2>
+                    <p class="pr-5 text-white-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac ante mollis quam tristique convallis </p>
+                </div>
+                <div class="col-lg-3 col-xs-12 links">
+                    <h4 class="mt-lg-0 mt-sm-3">Bizi takip edin</h4>
+                        <ul class="m-0 p-0">
+                        <li>- <a href="https://www.facebook.com/" target="blank">Facebook</a></li>
+                        <li>- <a href="https://www.instagram.com/" target="blank">İnstagram</a></li>
+                        <li>- <a href="https://twitter.com/login?lang=tr" target="blank">Twitter</a></li>
+                        <li>- <a href="https://web.telegram.org/" target="blank">Telegram</a></li>
+                        <li>- <a href="https://web.bip.com/" target="blank">Bip</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-xs-12 location">
+                    <h4 class="mt-lg-0 mt-sm-4">Adres ve İletişim Bilgileri</h4>
+                    <p>22, Lorem ipsum dolor, consectetur adipiscing</p>
+                    <p class="mb-0"><i class="fa fa-phone mr-3"></i>(541) 754-3010</p>
+                    <p><i class="fa fa-envelope-o mr-3"></i>info@hsdf.com</p>
+                </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col copyright">
+                        <p class=""><small class="text-white-50">© 2019. All Rights Reserved.</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+</main>
+
+<style>
+    a {
+        color: #909287;
+        text-decoration: none;
+    }
+    
+    a:hover {
+        color: #fff;
+    }
+</style>
--- frontend/src/views/homePage.svelte
+++ frontend/src/views/homePage.svelte
@@ -1,12 +1,19 @@
 <script>
-    import Navbar from '../components/navbar.svelte'
-    import TicketSection from '../components/ticketMenu.svelte'
+    import Navbar from '../components/navbar.svelte';
+    import TicketSection from '../components/ticketMenu.svelte';
+    import Footer from '../components/footer.svelte';
 </script>
 
 <main>
     <Navbar />
-    <TicketSection />
+
     <!-- BİLET SATIN ALMA BÖLÜMÜ-->
+    <TicketSection />
+
+    <!-- FOOTER -->
+    <Footer />
+    
+
 </main>
 
 <style>
