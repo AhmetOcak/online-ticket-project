ff6b6ccc70c0f713a418fc37e0d55e31af7c201d
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-11-20 14:31:52 +0300

    router eklendi

---
+++ frontend/src/router/routes.js
@@ -0,0 +1,9 @@
+import HomePage from '../views/homepage.svelte';
+import TicketCancellationPage from '../views/ticketCancellationPage.svelte';
+
+const routes = {
+    '/': HomePage,
+    '/ticketCancellationPage': TicketCancellationPage
+};
+
+export default routes;
