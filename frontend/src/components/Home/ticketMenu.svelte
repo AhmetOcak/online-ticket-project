d50a8bc3b225303f338fa492eed3f4dac44be4aa
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-11-20 16:50:38 +0300

    düzenleme

--- frontend/src/components/Home/ticketMenu.svelte
+++ frontend/src/components/Home/ticketMenu.svelte
@@ -1,27 +1,66 @@
 <script>
-    
+    let showCard = false;
+
+    const selectBus = () => {
+        showCard = true;
+    }
+
+    const selectPlane = () => {
+        showCard = false;
+    }
 </script>
 
 <main>
     <div class="ticketsection">
         <div class="searchbox">
-            <div class="card">
-                <form class="form fs-5">
-                    <label for="kalkis" class="fs-4 pb-1"><i class="bi bi-geo-alt-fill p-1"></i>Kalkış Noktası</label>
-                    <select id="kalkis" class="py-3 mb-3" >
-                        <option value="veritabanı">veritabanı</option>
-                        <option value="veritabanı">veritabanı</option>
-                    </select>
-                    <label for="varis" class="fs-4 pb-1"><i class="bi bi-geo-alt-fill p-1"></i>Varış Noktası</label>
-                    <select id="varis" class="py-3 mb-3">
-                        <option value="veritabanı">veritabanı</option>
-                        <option value="veritabanı">veritabanı</option>
-                    </select>
-                    <label for="tarih" class="fs-4"><i class="bi bi-calendar-fill p-1 pb-1"></i>Yolculuk Tarihi</label>
-                    <input type="date" id="tarih" class="p-3">
-                    <button type="button" class="btn btn-dark mt-4 p-3 fs-4" id="button">Bilet Bul</button>
-                </form>
-            </div>
+            <ul>
+                <li class="buttons">
+                    <button on:click={selectBus}>Otobüs</button>
+                    <button on:click={selectPlane}>Uçak</button>
+                    {#if showCard}
+                <li>
+                    <div class="card">
+                        <form class="form fs-5">
+                            <label for="kalkis" class="fs-4 pb-1"><i class="bi bi-geo-alt-fill p-1"></i>Kalkış Noktası</label>
+                            <select id="kalkis" class="py-3 mb-3" >
+                                <option value="veritabanı">veritabanı</option>
+                                <option value="veritabanı">veritabanı</option>
+                            </select>
+                            <label for="varis" class="fs-4 pb-1"><i class="bi bi-geo-alt-fill p-1"></i>Varış Noktası</label>
+                            <select id="varis" class="py-3 mb-3">
+                                <option value="veritabanı">veritabanı</option>
+                                <option value="veritabanı">veritabanı</option>
+                            </select>
+                            <label for="tarih" class="fs-4"><i class="bi bi-calendar-fill p-1 pb-1"></i>Yolculuk Tarihi</label>
+                            <input type="date" id="tarih" class="p-3">
+                            <button type="button" class="btn btn-dark mt-4 p-3 fs-4" id="button">Bilet Bul</button>
+                        </form>
+                    </div>
+                </li>
+                {/if}  
+                {#if !showCard}
+                <li>
+                    <div class="card">
+                        <form class="form fs-5">
+                            <label for="kalkis" class="fs-4 pb-1"><i class="bi bi-geo-alt-fill p-1"></i>Kalkış Noktası</label>
+                            <select id="kalkis" class="py-3 mb-3" >
+                                <option value="veritabanı">veritabanı</option>
+                                <option value="veritabanı">veritabanı</option>
+                            </select>
+                            <label for="varis" class="fs-4 pb-1"><i class="bi bi-geo-alt-fill p-1"></i>Varış Noktası</label>
+                            <select id="varis" class="py-3 mb-3">
+                                <option value="veritabanı">veritabanı</option>
+                                <option value="veritabanı">veritabanı</option>
+                            </select>
+                            <label for="tarih" class="fs-4"><i class="bi bi-calendar-fill p-1 pb-1"></i>Yolculuk Tarihi</label>
+                            <input type="date" id="tarih" class="p-3">
+                            <button type="button" class="btn btn-dark mt-4 p-3 fs-4" id="button">Bilet Bul</button>
+                        </form>
+                    </div>
+                </li>
+                {/if}
+                </li>
+            </ul>
         </div>
     </div>
 </main>
@@ -59,5 +98,16 @@
     #button:hover {
         background-color: #000;
         color: #fff;
+    }
+
+    li {
+        list-style: none;
+    }
+
+    .buttons {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 100%;
     }
 </style>
