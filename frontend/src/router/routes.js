927dfa479f1864268154fc0afebee2d773af4de0
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-11-21 15:22:34 +0300

    satın alma ekranı eklendi

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -2,12 +2,16 @@ import HomePage from '../views/homepage.svelte';
 import TicketCancellationPage from '../views/ticketCancellationPage.svelte';
 import BusTicketListPage from '../views/TicketListViews/busTicketListPage.svelte';
 import PlaneTicketListPage from '../views/TicketListViews/planeTicketListPage.svelte';
+import BuyBusTicketPage from '../views/BuyTicketViews/buyBusTicketPage.svelte';
+import BuyPlaneTicketPage from '../views/BuyTicketViews/buyPlaneTicketPage.svelte';
 
 const routes = {
     '/': HomePage,
     '/ticketCancellationPage': TicketCancellationPage,
     '/busTicketListPage': BusTicketListPage,
-    '/planeTicketListPage': PlaneTicketListPage
+    '/planeTicketListPage': PlaneTicketListPage,
+    '/buyBusTicketPage': BuyBusTicketPage,
+    '/buyPlaneTicketPage': BuyPlaneTicketPage
 };
 
 export default routes;
