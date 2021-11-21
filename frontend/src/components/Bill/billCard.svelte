d73441c4e6cc94192f05f7d328823998023818ec
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-11-21 21:13:21 +0300

    faturalarım sayfası düzenlendi

---
+++ frontend/src/components/Bill/billCard.svelte
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<main>
+    <div class="card text-light bg-secondary mb-3">
+        <div class="card-header">Company Name</div>
+        <div class="card-body">
+            <h5 class="card-title">Sayın Ahmet Ocak</h5>
+            <p class="card-text">BİLET ID : 324</p>
+            <p class="card-text">Fatura Tutarı : 175 TL</p>
+            <p class="card-text">İşlemin Yapıldığı Tarih : 01.12.2021 15:50</p>
+            <p class="card-text">İşlem Kanalının Adı : onlinebilet.com</p>
+        </div>
+    </div>
+</main>
+
+<style>
+    
+</style>
--- frontend/src/views/myBills.svelte
+++ frontend/src/views/myBills.svelte
@@ -1,11 +1,57 @@
 <script>
-
+    import Navbar from '../components/Navbar/navbar.svelte';
+    import BillCard from '../components/Bill/billCard.svelte';
 </script>
 
 <main>
-
+    <div class="section d-flex flex-column justify-content-center">
+        <Navbar />
+        <div class="container  d-flex flex-column justify-content-start align-items-center">
+            <h1 class="pt-3">Faturalarım</h1>
+            <ul class="list-group">
+                <li>
+                    <BillCard />
+                </li>
+                <li>
+                    <BillCard />
+                </li>
+                <li>
+                    <BillCard />
+                </li>
+                <li>
+                    <BillCard />
+                </li>
+            </ul>
+        </div>
+    </div>
 </main>
 
 <style>
-        
+    .section {
+        width: 100%;
+        height: 100vh;
+        background-image: url("../assets/billBG.png");
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+    li {
+        width: 800px;
+        background-color: rgb(255, 255,255, 0.7);
+        padding: 20px;
+        list-style: none;
+    }
+    
+    .container {
+        margin-top: 100px;
+        overflow: auto;
+        width: 900px;
+        height: 70vh;
+        background-color: rgb(255, 255,255, 0.5);
+    }
+
+    ul {
+        padding-top: 50px;
+        padding-bottom: 50px;
+    }
 </style>
