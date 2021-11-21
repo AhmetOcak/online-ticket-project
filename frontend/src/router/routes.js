726edcd35a7a3003a35be3233b83b81cc77a15a0
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-11-21 12:40:12 +0300

    bilet listeleme ekranı hazırlandı

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,9 +1,11 @@
 import HomePage from '../views/homepage.svelte';
 import TicketCancellationPage from '../views/ticketCancellationPage.svelte';
+import TicketList from '../views/ticketList.svelte';
 
 const routes = {
     '/': HomePage,
-    '/ticketCancellationPage': TicketCancellationPage
+    '/ticketCancellationPage': TicketCancellationPage,
+    '/ticketList': TicketList
 };
 
 export default routes;
