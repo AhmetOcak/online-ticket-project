b8974ce1430f0f2a0bca852886664e33b5323821
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-11-24 12:00:27 +0300

    düzenleme yapılacak

--- frontend/src/components/BuyTicket/buyTicket.svelte
+++ frontend/src/components/BuyTicket/buyTicket.svelte
@@ -11,7 +11,7 @@
 
 <main>
     <div class="d-flex flex-row justify-content-between">
-        <!--
+        
             <div class="card text-dark bg-light mb-3" id="passengerInfo">
             <div class="card-header">Yolcu Bilgileri</div>
             <div class="">
@@ -28,7 +28,7 @@
                 <button type="button" class="btn btn-success fs-5">{buttonText}</button>
             </div>
         </div>
-        -->
+        
         <div class="space p-3"></div>
         <div class="card text-dark bg-light mb-3" id="cardInfo">
             <div class="card-header">Ödeme Bilgileri</div>
