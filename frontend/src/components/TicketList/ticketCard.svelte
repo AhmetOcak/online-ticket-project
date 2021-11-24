42b009e259b336ee1d3c8a855ca4305641bf4ad9
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-11-25 00:54:09 +0300

    bilet iptal butonu eklendi

--- frontend/src/components/TicketList/ticketCard.svelte
+++ frontend/src/components/TicketList/ticketCard.svelte
@@ -7,11 +7,20 @@
     export let arrivalTime;
     export let price;
     export let pushPage;
+    export let departurePlace;
+    export let arrivalPlace;
+    export let showButton = false;
+    export let cancelTheTicket = false;
 </script>
 
 <main>
     <div class="card text-dark bg-light mb-3">
-        <div class="card-header fs-4">{companyName}</div> <!-- Şirket ismi -->
+        <div class="card-header fs-4 d-flex flex-row justify-content-between">
+            {companyName}
+            {#if cancelTheTicket}
+                <button type="button" class="btn btn-danger p-3 fs-5">Bileti iptal et</button>
+            {/if}
+        </div>
         <div class="card-body d-flex flex-row justify-content-between">
             <div class="d-flex align-item-center">
                 <div class="d-flex flex-column justify-content-center align-items-center">
@@ -37,8 +46,22 @@
                     <p>{price}</p>
                 </div>
             </div>
-            <button type="button" class="btn btn-dark" on:click={() => push(pushPage)}>Seç</button>
+            {#if showButton} 
+                <button type="button" class="btn btn-dark" id="pushButton" on:click={() => push(pushPage)}>Seç</button>
+            {/if}
         </div>
+        {#if cancelTheTicket}
+            <div class="d-flex flex-row justify-content-around">
+                <div class="d-flex flex-column justify-content-center align-item-center">
+                    <h4>Kalkış Yeri</h4>
+                    <p>{departurePlace}</p>
+                </div>
+                <div class="d-flex flex-column justify-content-center align-item-center">
+                    <h4>Varış Yeri</h4>
+                    <p>{arrivalPlace}</p>
+                </div>
+            </div>
+        {/if}
     </div>
 </main>
 
@@ -47,7 +70,7 @@
         font-size: 20px;
     }
 
-    button {
+    #pushButton {
         width: 80px;
         border-top-right-radius: 100%;
         border-bottom-right-radius: 100%;
