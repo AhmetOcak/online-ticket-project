ac4ebccd8c9b2708299e25d7069dea43e1003f3a
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-11-25 01:40:13 +0300

    modal eklendi

---
+++ frontend/src/components/TicketSearchResults/ticketNotFound.svelte
@@ -0,0 +1,25 @@
+<script>
+    
+</script>
+
+<main>
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title text-danger" id="staticBackdropLabel">Rezervasyon Bulunamadı</h4>
+                </div>
+                <div class="modal-body fs-5">
+                    Girdiğiniz kriterlere uygun bir rezervasyon bulamadık.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Tamam</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<style>
+
+</style>
